1eb480d2cd989691a4d9428aad5a9b81276d9989
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2021-04-17 22:07:26 +0530

    Overloading and Overriding and super keyword on April17th 2021

--- src/main/java/com/java/programs/overloadMain.java
+++ src/main/java/com/java/programs/overloadMain.java
@@ -2,22 +2,28 @@ package com.java.programs;
 
 public class overloadMain {
 
-	
 	public static void main(String a[]) {
 		System.out.println("main method");
-
 		main(10);
 		main(10, 20);
 	}
 
 	public static void main(int a) {
 		System.out.println("main method " + a);
-
 	}
 
 	public static void main(int a, int b) {
 		System.out.println("main method " + a + b);
-
 	}
 
+	// Below main methods are not executed/called by JVM because JVM calls the
+	// main method which is having String array of arguments as parameter
+	public static void main(String args) {
+		System.out.println("main with String");
+	}
+
+	public static void main() {
+		System.out.println("main without args");
+	}
+	
 }
