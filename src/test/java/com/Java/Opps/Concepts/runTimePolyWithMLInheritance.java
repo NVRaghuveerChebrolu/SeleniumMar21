49cdcc2cedcc402aac650af4a9015d22443bc528
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2021-04-20 23:38:08 +0530

    dynamic binding or run time polymorphism 20 april 2021

---
+++ src/test/java/com/Java/Opps/Concepts/runTimePolyWithMLInheritance.java
@@ -0,0 +1,24 @@
+package com.Java.Opps.Concepts;
+
+class Animal7457 {
+	void eat() {
+		System.out.println("Animal7457 is eating...");
+	}
+}
+
+class dog1234 extends Animal7457 {
+	void eat() {
+		System.out.println("dog is eating...");
+	}
+}
+
+public class runTimePolyWithMLInheritance extends dog1234 {
+	/*
+	 * void eat() { System.out.println(
+	 * "runTimePolyWithMLInheritance is eating..."); }
+	 */
+	public static void main(String args[]) {
+		Animal7457 a = new runTimePolyWithMLInheritance();
+		a.eat();
+	}
+}
