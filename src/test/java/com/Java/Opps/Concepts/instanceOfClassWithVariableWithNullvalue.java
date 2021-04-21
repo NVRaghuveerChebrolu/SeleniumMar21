98cfe77d248f1652e99435553eaf69c190f555b5
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2021-04-21 23:47:32 +0530

    static and dynamic binding, instance of operator on April 21 2021

---
+++ src/test/java/com/Java/Opps/Concepts/instanceOfClassWithVariableWithNullvalue.java
@@ -0,0 +1,8 @@
+package com.Java.Opps.Concepts;
+
+public class instanceOfClassWithVariableWithNullvalue {
+	public static void main(String args[]) {
+		instanceOfClassWithVariableWithNullvalue d = null;
+		System.out.println(d instanceof instanceOfClassWithVariableWithNullvalue);// false
+	}
+}
