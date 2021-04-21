6d81e3c2ba0d09b0d510d57773d570b18dee033a
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2021-04-21 23:58:39 +0530

    allign programs in src/test/java package on april 21

---
+++ src/test/java/com/Java/Program/ClassAndObject/classAndObjectInitThroughMethod.java
@@ -0,0 +1,26 @@
+package com.Java.Program.ClassAndObject;
+
+class student123 {
+	int rollno;
+	String name;
+
+	void insertRecord(int r, String n) {
+		rollno = r;
+		name = n;
+	}
+
+	void displayInformation() {
+		System.out.println(rollno + " " + name);
+	}
+}
+
+ class classAndObjectInitThroughMethod {
+	public static void main(String args[]) {
+		student123 s1 = new student123();
+		student123 s2 = new student123();
+		s1.insertRecord(111, "Karan");
+		s2.insertRecord(222, "Aryan");
+		s1.displayInformation();
+		s2.displayInformation();
+	}
+}
---
+++ src/test/java/com/Java/Program/ClassAndObject/classAndObjectInitThroughRef.java
@@ -0,0 +1,16 @@
+package com.Java.Program.ClassAndObject;
+
+class TestStudent2 {
+	int id;
+	String name;
+}
+
+public class classAndObjectInitThroughRef {
+	public static void main(String args[]) {
+		TestStudent2 s1 = new TestStudent2();
+		s1.id = 101;
+		s1.name = "Sonoo";
+		//// printing members with a white space
+		System.out.println(s1.id + " " + s1.name);
+	}
+}
