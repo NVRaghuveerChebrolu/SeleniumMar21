3c169cb631b4594cd6edc21a61c5e33ba8971cf2
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2021-05-06 00:12:17 +0530

    committing the code for testNG Concepts - TestNG Factory, TestNg Data provider, parallel test cases,parallel test classes,Groups..etc on May05 2021

--- src/test/java/com/testNG/concepts/testNgDataProvider.java
+++ src/test/java/com/testNG/concepts/testNgDataProvider.java
@@ -9,6 +9,8 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
 
 public class testNgDataProvider {
@@ -25,9 +27,24 @@ public class testNgDataProvider {
 		System.out.println("pwd: " + pwd);
 
 	}
+	
+	@Test 
+	public void test2 (){
+		System.out.println("inside test2");
+	}
 
 	@DataProvider
 	public Object[][] dp() {
 		return new Object[][] { new Object[] { "user1", "pwd1" }, new Object[] { "user2", "pwd2" }, };
+	}
+	
+	@BeforeMethod
+	public void BeforeMethod(){
+		System.out.println("inside BeforeMethod");
+	}
+	
+	@AfterMethod
+	public void AfterMethod(){
+		System.out.println("inside AfterMethod");
 	}
 }
